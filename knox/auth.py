9e46c0a5f776e0385ec3275ea064f8d58d5a9ea4
kdb621 <kdb621@nexivil.com>
2020-06-01 10:20:11 +0900

    Reduce queries

--- knox/auth.py
+++ knox/auth.py
@@ -64,7 +64,7 @@ class TokenAuthentication(BaseAuthentication):
         '''
         msg = _('Invalid token.')
         token = token.decode("utf-8")
-        for auth_token in AuthToken.objects.filter(
+        for auth_token in AuthToken.objects.select_related('user').filter(
                 token_key=token[:CONSTANTS.TOKEN_KEY_LENGTH]):
             if self._cleanup_token(auth_token):
                 continue
